8a712ee21ef640652ba63ad21475a80671293d76
avenj <avenj@cobaltirc.org>
2012-08-24 15:39:54 -0400

    Purge smart-match operator.
    	modified:   lib/Bot/Cobalt/Plugin/Silly/AutoOpAll.pm

--- lib/Bot/Cobalt/Plugin/Silly/AutoOpAll.pm
+++ lib/Bot/Cobalt/Plugin/Silly/AutoOpAll.pm
@@ -97,7 +97,7 @@ sub Bot_user_joined {
 
   $chan = lc_irc($chan, $casemap);
 
-  if ($chan ~~ [ keys %{ $self->{AOPChans}->{$context} } ]) {
+  if (grep { $_ eq $chan } keys %{ $self->{AOPChans}->{$context} }) {
     $core->send_event( 'mode', $context, $chan, '+o '.$nick );
   }
 
